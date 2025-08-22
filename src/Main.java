cdd724d02c95042a86f30abcfbec813c653dd758
Anderson Matheus <a.matheus.dev@gmail.com>
2025-08-22 12:01:00 -0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Valor do Empréstimo: ");
+        double GetValorEmprestimo = Double.parseDouble(scanner.nextLine().trim());
+        System.out.print("Taxa de juros anual: ");
+        float GetTaxaJurosAnual =  Float.parseFloat(scanner.nextLine().trim());
+        System.out.print("Periodo (Anos): ");
+        double GetPeriodoAnos =  Double.parseDouble(scanner.nextLine().trim());
+        double TaxaJurosMensal = (double)(GetTaxaJurosAnual/100)/12;
+        double PeriodoMes = GetPeriodoAnos * 12;
+        //double PrimeiroResultado = Math.pow((1 + TaxaJurosMensal), PeriodoMes);
+        // double SegundoResultado = Math.pow((1 + TaxaJurosMensal), PeriodoMes);
+        // double FinalResultado = (TaxaJurosMensal*PrimeiroResultado)/(SegundoResultado - 1);
+        double Mortgage = GetValorEmprestimo*(TaxaJurosMensal*(Math.pow((1 + TaxaJurosMensal), PeriodoMes))/((Math.pow((1 + TaxaJurosMensal), PeriodoMes)) - 1));
+        String MortgageCurrency = NumberFormat.getCurrencyInstance().format(Mortgage);
+        System.out.println("Mortgage: " + MortgageCurrency);
+    }
+}
